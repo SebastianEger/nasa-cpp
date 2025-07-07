92b8d0a7da839a01ebf82651071e8e76249fa54a
Sebastian Eger <eger.sebastian@gmx.de>
2025-07-07 08:26:57 +0200

    update example

--- src/nasa_msg_handler.cpp
+++ src/nasa_msg_handler.cpp
@@ -10,6 +10,45 @@ namespace nasacpp
     NasaMsgHandler::NasaMsgHandler(const MessageNumber &message_number, const Address &sa, const DataType &data_type)
         : message_number_(message_number), sa_(sa), data_type_(data_type), msg_set_type_(getMessageSetType(message_number)) {}
 
+    void NasaMsgHandler::getPackets(std::vector<Packet> &pkts)
+    {
+        // send READ packet if value is not initialized yet
+        if (val_is_ == INT_MAX && initial_read_out)
+        {
+            pkts.push_back(Packet::createToBroadcastSelf(sa_, DataType::Read, message_number_, 1));
+            return;
+        }
+
+        // no need to send packet
+        if (val_new_ == INT_MAX || val_is_ == val_new_ || data_type_ == DataType::Read)
+        {
+            return;
+        }
+
+        // check if send packet is disabled by user config
+        if (data_type_ == DataType::Write && disable_write_send)
+        {
+            spdlog::warn("NasaMsgHandler: [SEND] disabled for [WRITE]!");
+            val_new_.store(val_is_);
+            resetRetryCounter();
+            return;
+        }
+
+        // check if val_new is set and retry counter not reached
+        if (retry_ctr_ > 10)
+        {
+            spdlog::warn("NasaMsgHandler: [SEND] retry counter reached!");
+            val_new_.store(val_is_);
+            resetRetryCounter();
+            return;
+        }
+
+        // create packet
+        auto pkt = Packet::createToBroadcastSelf(sa_, data_type_, message_number_, val_new_);
+        pkt.command.retry_count = retry_ctr_++;
+        pkts.push_back(pkt);
+    }
+
     void NasaMsgHandler::updateValue(const MessageSet &msg)
     {
         // check message number
@@ -39,7 +78,7 @@ namespace nasacpp
         }
 
         // reset value new
-        if(val_is_ == val_new_)
+        if (val_is_ == val_new_)
         {
             val_new_ = INT_MAX;
         }
@@ -48,41 +87,6 @@ namespace nasacpp
     void NasaMsgHandler::updateValueNew(const int &val)
     {
         val_new_ = val;
-    }
-
-    void NasaMsgHandler::getPackets(std::vector<Packet> &pkts)
-    {
-        // send READ packet if value is not initialized yet
-        if (val_is_ == INT_MAX && initial_read_out)
-        {
-            pkts.push_back(Packet::createToBroadcastSelf(sa_, DataType::Read, message_number_, 1));
-            return;
-        }
-
-        // no need to send packet
-        if (val_new_ == INT_MAX || val_is_ == val_new_ || data_type_ == DataType::Read)
-        {
-            return;
-        }
-
-        // check if send packet is disabled by user config
-        if (data_type_ == DataType::Write && disable_write_send)
-        {
-            spdlog::warn("NasaMsgHandler: [SEND] disabled for [WRITE]!");
-            return;
-        }
-
-        // check if val_new is set and retry counter not reached
-        if (retry_ctr_ > 10)
-        {
-            spdlog::warn("NasaMsgHandler: [SEND] retry counter reached!");
-            return;
-        }
-
-        // create packet
-        auto pkt = Packet::createToBroadcastSelf(sa_, data_type_, message_number_, val_new_);
-        pkt.command.retry_count = retry_ctr_++;
-        pkts.push_back(pkt);
     }
 
     const DataType &NasaMsgHandler::getDataType() const
